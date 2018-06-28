3360cf499de520152f3630974de334f9e1925052
manoj007kumar <manoj007.mahadevaiah@gmail.com>
2018-06-28 20:42:38 +0530

    Prime file created

---
+++ Prime.java
@@ -0,0 +1,24 @@
+class Prime
+{
+   public static void main(String[] args)
+      {
+           boolean flag=false
+           int n=15;
+           for(int i=2;i<=n/2;i++)
+           {
+              if(n%i==0)
+              {
+                  boolean=true;
+                  break;
+               }
+            }
+            if(flag)
+            {
+                System.out.println("not a prime");
+             }
+             else
+             {
+                System.out.println("prime");
+              }
+          }
+  }        
